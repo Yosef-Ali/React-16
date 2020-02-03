2296b42e39e2d4abc9678c419d3709da71c78238
apple <apple@apples-MacBook-Pro.local>
2020-02-03 15:13:42 +0300

    add props to the component

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<Welcome text="Welcome to Using Props" />
+				</header>
+				<p className="App-intro">
+					To get started, edit <code>src/App.js</code> and save to reload.
+				</p>
+			</div>
+		);
+	}
+}
+
+class Welcome extends Component {
+	render() {
+		const { text } = this.props;
+		return <h1 className="App-title">{text}</h1>;
+	}
 }
 
 export default App;
