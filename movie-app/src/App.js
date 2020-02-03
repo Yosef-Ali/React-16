b2222950fd67fc1d2cf5b8ba612aefeba2938382
apple <apple@apples-MacBook-Pro.local>
2020-02-03 20:56:35 +0300

    PropType, isRequiered, defaultProps

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -2,51 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Movie from './Movie';
+
+const movies = [
+	{
+		id: 1,
+		title: 'Star War',
+		desc: 'A space movie'
+	},
+	{
+		id: 2,
+		title: 'Sprider Man'
+	},
+	{
+		id: 3,
+		title: 'Man in Black'
+	}
+];
+
 class App extends Component {
-	state = {
-		input: 'Hello'
-	};
-
-	updateInput = event => {
-		this.setState({
-			input: event.target.value.trim()
-		});
-	}; // two way binding, updating input filed
-
-	submit = () => {
-		console.log(this.text.value);
-	}; /* "this.text.value" is added to get the input value */
-
 	render() {
 		return (
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
-					<Welcome text="Welcome to Using Props" />
 				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
-				<h3>{this.state.input}</h3> {/** Two day binding */}
-				<input
-					type="text"
-					onChange={this.updateInput}
-					value={this.state.input}
-				/>
-				{/* this.updateInput calls method, this.sate.input assign value */}
-				<input type="text" ref={input => (this.text = input)} />
-				{/*  ref is used for input with arrow function */}
-				<button onClick={this.submit}>Show Value</button>
+				{movies.map(movie => (
+					<Movie key={movie.id} movie={movie} desc={movie.desc} />
+				))}
 			</div>
 		);
-	}
-}
-
-class Welcome extends Component {
-	render() {
-		const { text } = this.props;
-
-		return <h1 className="App-title">{text}</h1>;
 	}
 }
 
