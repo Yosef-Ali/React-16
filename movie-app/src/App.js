e687a244771ef05d9330782ea6e3eb6530602d82
apple <apple@apples-MacBook-Pro.local>
2020-02-03 16:14:50 +0300

    state,method,conditinals & passing props to chiled

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -3,16 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+	state = {
+		toggle: true
+	};
+
+	toggle = () => {
+		this.setState({
+			toggle: !this.state.toggle
+		});
+	}; /* method */
+
 	render() {
 		return (
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
-					<Welcome text="Welcome to Using Props" />
+					<Welcome
+						text="Welcome to Using Props"
+						toggle={this.state.toggle}
+					/>{' '}
+					{/* passing props to chiled */}
 				</header>
 				<p className="App-intro">
 					To get started, edit <code>src/App.js</code> and save to reload.
 				</p>
+				{this.state.toggle && <p>This is show and hide</p>} {/* Conditional */}
+				<button onClick={this.toggle}>Show/Hide</button>{' '}
+				{/* "this.toggle" method called here */}
 			</div>
 		);
 	}
@@ -20,7 +37,8 @@ class App extends Component {
 
 class Welcome extends Component {
 	render() {
-		const { text } = this.props;
+		const { text, toggle } = this.props;
+		console.log(toggle);
 		return <h1 className="App-title">{text}</h1>;
 	}
 }
