6958152d29c879b45554814ee72b3354c4ba1232
apple <apple@apples-MacBook-Pro.local>
2020-02-03 18:43:44 +0300

    Controled input, two way binding

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+	state = {
+		input: 'Hello'
+	};
+
+	updateInput = event => {
+		this.setState({
+			input: event.target.value.trim()
+		});
+	}; // two way binding, updating input filed
+
 	submit = () => {
 		console.log(this.text.value);
 	}; /* "this.text.value" is added to get the input value */
@@ -17,7 +27,14 @@ class App extends Component {
 				<p className="App-intro">
 					To get started, edit <code>src/App.js</code> and save to reload.
 				</p>
-				<input type="text" ref={input => (this.text = input)} />{' '}
+				<h3>{this.state.input}</h3> {/** Two day binding */}
+				<input
+					type="text"
+					onChange={this.updateInput}
+					value={this.state.input}
+				/>
+				{/* this.updateInput calls method, this.sate.input assign value */}
+				<input type="text" ref={input => (this.text = input)} />
 				{/*  ref is used for input with arrow function */}
 				<button onClick={this.submit}>Show Value</button>
 			</div>
